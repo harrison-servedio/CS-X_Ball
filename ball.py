2f33a062327c98bbf356681203d4110dbbda2712
hs242424 <79582976+harrison-servedio@users.noreply.github.com>
2024-09-11 11:53:00 -0400

    did stuff

--- ball.py
+++ ball.py
@@ -1,17 +1,15 @@
 import random as r
 import math
 
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
 
 class ball:
     
-    def __init__(self) -> None:
+    def __init__(self, color) -> None:
         self.Xs = [2]
         self.Ys = [2]
         
-        self.radius = 1
+        self.color = color
         
     def stepToCenter(self, minVelo = 5, maxVelo = 100):
         size = r.randint(0, 180)
@@ -23,21 +21,3 @@ class ball:
 
         self.Xs.append(self.Xs[-1] + velo*math.cos(travelAngle*math.pi/180))
         self.Ys.append(self.Ys[-1] + velo*math.sin(travelAngle*math.pi/180))
-
-        
-a = ball()
-
-
-def animate(i):
-    plt.cla()
-    a.stepToCenter()
-    # plt.plot(a.Xs, a.Ys)
-    plt.plot(a.Xs[-1], a.Ys[-1], marker="o", markersize=20, markeredgecolor="red", markerfacecolor="green")
-    plt.
-    plt.axis('equal')
-
-fig, ax = plt.subplots()
-
-ani = FuncAnimation(plt.gcf(), animate, interval=200, cache_frame_data=False)
-
-plt.show()
